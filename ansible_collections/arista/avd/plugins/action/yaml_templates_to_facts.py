c215ef8200bc733d78f0d23f63c371db6ad0c7f1
Claus Holbech <holbech@arista.com>
2021-12-01 16:49:00 +0100

    add debug timestamps

--- ansible_collections/arista/avd/plugins/action/yaml_templates_to_facts.py
+++ ansible_collections/arista/avd/plugins/action/yaml_templates_to_facts.py
@@ -8,7 +8,7 @@ from ansible.utils.vars import isidentifier
 from ansible.plugins.filter.core import combine
 from ansible.plugins.loader import lookup_loader
 from ansible_collections.arista.avd.plugins.module_utils.strip_empties import strip_null_from_data
-
+from datetime import datetime;
 
 class ActionModule(ActionBase):
     def run(self, tmp=None, task_vars=None):
@@ -37,6 +37,9 @@ class ActionModule(ActionBase):
                     raise AnsibleActionFail("The argument 'templates' is not a list")
             else:
                 raise AnsibleActionFail("The argument 'templates' must be set")
+
+            debug = self._task.args.get("debug",False)
+
         else:
             raise AnsibleActionFail("The argument 'templates' must be set")
 
@@ -46,13 +49,21 @@ class ActionModule(ActionBase):
 
         template_vars = task_vars
 
+        if debug:
+            avd_yaml_templates_to_facts_debug = template_vars.get('avd_yaml_templates_to_facts_debug',[])
+
         for template_item in template_list:
+            if debug:
+                debug_item = template_item
+                debug_item['timestamps'] = { "starting": datetime.now() }
+                
             template = template_item.get('template')
             if not template:
                 raise AnsibleActionFail("Invalid template data")
 
             template_options = template_item.get('options', {})
             list_merge = template_options.get('list_merge', 'append')
+
             strip_empty_keys = template_options.get('strip_empty_keys', True)
 
             if root_key:
@@ -60,12 +71,33 @@ class ActionModule(ActionBase):
             else:
                 template_vars = combine(task_vars, output, recursive=True)
 
+            if debug:
+                debug_item['timestamps']['run_template'] = datetime.now()
+
             template_output = template_lookup_module.run([template], template_vars)
+            if debug:
+                debug_item['timestamps']['load_yaml'] = datetime.now()
+
             template_output_data = yaml.safe_load(template_output[0])
+
             if strip_empty_keys:
+                if debug:
+                    debug_item['timestamps']['strip_empty_keys'] = datetime.now()
+
                 template_output_data = strip_null_from_data(template_output_data)
+
             if template_output_data:
+                if debug:
+                    debug_item['timestamps']['combine_data'] = datetime.now()
+
                 output = combine(output, template_output_data, recursive=True, list_merge=list_merge)
+
+            if debug:
+                debug_item['timestamps']['done'] = datetime.now()
+                avd_yaml_templates_to_facts_debug.append(debug_item)
+
+        if debug:
+            output['avd_yaml_templates_to_facts_debug'] = avd_yaml_templates_to_facts_debug
 
         if root_key:
             result['ansible_facts'] = {root_key: output}
