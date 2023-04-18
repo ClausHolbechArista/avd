fd4f917f326fa7a2258ba6df8c3947e27fb88fe2
Claus Holbech <holbech@arista.com>
2023-04-18 08:46:12 +0200

    Feat(plugins): Lookup plugin arista.avd.get_node_config

---
+++ ansible_collections/arista/avd/plugins/lookup/get_node_config.py
@@ -0,0 +1,43 @@
+# python 3 headers, required if submitting to Ansible
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+    name: get_node_config
+    author: Arista Ansible Team (@aristanetworks)
+    version_added: "4.0.0"
+    short_description: Get AVD node config from Fabric Topology inputs, using same logic as eos_designs.
+    description:
+        - This lookup returns the contents of one or more AVD node config keys.
+        - Node config is the internal data set for a given node, after reading "Fabric Topology" inputs and implementing inheritance etc.
+        - If the value is not set, None/null will be returned.
+    options:
+        _terms:
+            description: List of keys to be read and returned. Supports dot-notation for deeper lookups.
+            required: True
+    notes:
+        - By default the lookup will read the variables for the current node.
+        - It is possible to read variables for another by setting variables=hostvars[<other hostname>]
+"""
+from ansible.plugins.lookup import LookupBase, display
+
+from ansible_collections.arista.avd.plugins.plugin_utils.eos_designs_shared_utils import SharedUtils
+from ansible_collections.arista.avd.plugins.plugin_utils.utils import get
+
+
+class LookupModule(LookupBase):
+    def run(self, terms, variables=None, **kwargs):
+        # First of all populate options,
+        # this will already take into account env vars and ini config
+        self.set_options(var_options=variables, direct=kwargs)
+
+        ret = []
+        avd_shared_utils = SharedUtils(variables, self._templar)
+        node_config = avd_shared_utils.switch_data_combined
+
+        for term in terms:
+            display.debug(f"get_node_config lookup term: {term}")
+            ret.append(get(node_config, term))
+
+        return ret
