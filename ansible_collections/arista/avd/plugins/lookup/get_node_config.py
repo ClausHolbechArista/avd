6daf399939a065580ee4c11d3c56a77a0a6ba1e4
Claus Holbech <holbech@arista.com>
2023-04-18 11:17:49 +0200

    Use get_node_config in fabric documentation

--- ansible_collections/arista/avd/plugins/lookup/get_node_config.py
+++ ansible_collections/arista/avd/plugins/lookup/get_node_config.py
@@ -11,17 +11,25 @@ DOCUMENTATION = r"""
     description:
         - This lookup returns the contents of one or more AVD node config keys.
         - Node config is the internal data set for a given node, after reading "Fabric Topology" inputs and implementing inheritance etc.
-        - If the value is not set, None/null will be returned.
+        - If the requested key is not set, None/null will be returned.
+        - If no keys are requested (no argument) the entire node_config dict will be returned.
     options:
         _terms:
             description: List of keys to be read and returned. Supports dot-notation for deeper lookups.
             required: True
+        hostname:
+            type: str
+            description:
+                - By default the lookup will read the variables for the current node.
+                - If hostname is set, the lookup will access hostvars of the given node.
+            required: False
     notes:
-        - By default the lookup will read the variables for the current node.
-        - It is possible to read variables for another by setting variables=hostvars[<other hostname>]
+        -
+        - It is possible to read variables for another by setting hostname=<other hostname>
 """
-from copy import deepcopy
+from collections import ChainMap
 
+from ansible.errors import AnsibleLookupError
 from ansible.plugins.lookup import LookupBase, display
 
 from ansible_collections.arista.avd.plugins.plugin_utils.eos_designs_shared_utils import SharedUtils
@@ -35,20 +43,32 @@ class LookupModule(LookupBase):
         # this will already take into account env vars and ini config
         self.set_options(var_options=variables, direct=kwargs)
 
+        default_hostname = variables["inventory_hostname"]
+        hostname = self.get_option("hostname")
+        if hostname is not None and hostname != default_hostname:
+            hostvars = ChainMap(variables["hostvars"].get(hostname))
+        else:
+            hostname = default_hostname
+            hostvars = ChainMap(variables["hostvars"].get(hostname))
+
         ret = []
         avd_schema_tools = AvdSchemaTools(
-            variables["inventory_hostname"],
-            display,
+            hostname=hostname,
+            ansible_display=display,
             schema_id="eos_designs",
             conversion_mode="debug",
             validation_mode="error",
             plugin_name="get_node_config",
         )
-        hostvars = deepcopy(variables)
-        avd_schema_tools.convert_and_validate_data(hostvars)
+        result = avd_schema_tools.convert_and_validate_data(hostvars)
+        if result.get("failed"):
+            raise AnsibleLookupError(result.get("msg"))
+
         avd_shared_utils = SharedUtils(hostvars, self._templar)
         node_config = avd_shared_utils.switch_data_combined
 
+        if not terms:
+            return [node_config]
         for term in terms:
             display.debug(f"get_node_config lookup term: {term}")
             ret.append(get(node_config, term))
