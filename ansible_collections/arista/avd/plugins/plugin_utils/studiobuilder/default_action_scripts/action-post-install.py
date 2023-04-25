a5da2dca6c07717924b05182a69493f704f1f6bb
Claus Holbech <holbech@arista.com>
2023-04-25 12:41:50 +0200

    .

--- ansible_collections/arista/avd/plugins/plugin_utils/studiobuilder/default_action_scripts/action-post-install.py
+++ ansible_collections/arista/avd/plugins/plugin_utils/studiobuilder/default_action_scripts/action-post-install.py
@@ -1,31 +1,33 @@
 import requests
 
+hooks = [
+    ("BUILD_STAGE_STUDIO_PRE_BUILD", ctx.action.args["StudioPreBuildActionIDs"].split(",")[0]),
+    ("BUILD_STAGE_WORKSPACE_PRE_BUILD", ctx.action.args["WorkspacePreBuildActionIDs"].split(",")[0]),
+    ("BUILD_STAGE_STUDIO_PRE_RENDER", ctx.action.args["StudioPreRenderActionIDs"].split(",")[0]),
+]
+
 cookies = {"access_token": ctx.user.token}
 
 ctx.alog("Postinstall script trying to associate build actions")
 # ctx.alog(f"args: {ctx.action.args}")
 
-url = f"https://{ctx.connections.serviceAddr}/api/resources/studio/v1/StudioConfig"
-data = {
-    "key": {
-        "workspace_id": ctx.action.args["WorkspaceID"],
-        "studio_id": ctx.action.args["StudioID"],
-    },
-    "studioPreBuildActions": {
-        "values": ctx.action.args["StudioPreBuildActionIDs"].split(","),
-    },
-    "studioPreRenderActions": {
-        "values": ctx.action.args["StudioPreRenderActionIDs"].split(","),
-    },
-    "workspacePreBuildActions": {
-        "values": ctx.action.args["WorkspacePreBuildActionIDs"].split(","),
-    },
-}
+url = f"https://{ctx.connections.serviceAddr}/api/resources/studio/v1/BuildHookConfig"
 
-response = requests.post(url, json=data, verify=False, cookies=cookies)
+for stage, action in hooks:
+    data = {
+        "key": {
+            "workspaceId": ctx.action.args["WorkspaceID"],
+            "studioId": ctx.action.args["StudioID"],
+            "stage": stage,
+            "hookId": "something",
+        },
+        "actionId": action,
+    }
 
-if response.status_code != 200:
-    ctx.alog("Postinstall script failure suring association of build actions")
-    raise RuntimeError(response.text)
+    response = requests.post(url, json=data, verify=False, cookies=cookies)
+
+    if response.status_code != 200:
+        ctx.alog("Postinstall script failure suring association of build actions")
+        raise RuntimeError(response.text)
 
 ctx.alog("Postinstall script done.")
