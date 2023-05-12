2eb8b4a940556bb86e395cff069ddd3facd78eed
Claus Holbech <holbech@arista.com>
2023-05-12 08:16:54 +0200

    Add copyrights & skip pylint

--- ansible_collections/arista/avd/plugins/plugin_utils/studiobuilder/default_action_scripts/action-studio-prebuild.py
+++ ansible_collections/arista/avd/plugins/plugin_utils/studiobuilder/default_action_scripts/action-studio-prebuild.py
@@ -1,3 +1,8 @@
+# Copyright (c) 2022 Arista Networks, Inc.  All rights reserved.
+# Arista Networks, Inc. Confidential and Proprietary.
+# Subject to Arista Networks, Inc.'s EULA.
+# FOR INTERNAL USE ONLY. NOT FOR DISTRIBUTION.
+# pylint: skip-file
 """
 action-studio-prebuild.py
 
--- ansible_collections/arista/avd/plugins/plugin_utils/studiobuilder/default_action_scripts/studio-template.mako
+++ ansible_collections/arista/avd/plugins/plugin_utils/studiobuilder/default_action_scripts/studio-template.mako
@@ -1,4 +1,9 @@
 <%
+# Copyright (c) 2022 Arista Networks, Inc.  All rights reserved.
+# Arista Networks, Inc. Confidential and Proprietary.
+# Subject to Arista Networks, Inc.'s EULA.
+# FOR INTERNAL USE ONLY. NOT FOR DISTRIBUTION.
+# pylint: skip-file
 """
 studio-template.mako
 
